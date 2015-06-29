4b95514bc929df466d5f806050e87d2cedbf1524
Seiki Tokunaga <otokuna46@gmail.com>
2015-06-30 01:58:21 +0900

    ユーザの登録まで
    ユーザ認証を追加した。フラッシュのメッセージも入れた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,11 +26,13 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+    
     respond_to do |format|
       if @user.save
+        flash[:success] = "Welcome to the UrlMarks"
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
+        #redirect_to @user
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -70,6 +72,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-     # params.require(:user).permit(:name, :, :password, :, :email)
+       params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 end
